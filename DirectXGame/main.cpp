4994452750954fe93e4fea7d758f96b4b991d32b
mochiyuki0403 <146901463+mochiyuki0403@users.noreply.github.com>
2024-09-02 00:01:46 +0900

    a

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"GC2D_11_モチダ_ユキヤ_AL3");
+	win->CreateGameWindow(L"GC2D_11_モチダ_ユキヤ_AL");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
